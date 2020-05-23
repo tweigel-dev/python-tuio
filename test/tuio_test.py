7624bcc6f5c627483fce76a5c36d29978b912213
Thomas Weigel <thomas.weigel@telekom.de>
2020-05-23 23:59:11 +0200

    merge test file

--- test/tuio_test.py
+++ test/tuio_test.py
@@ -37,28 +37,6 @@ def test_cursor():
         print("sented message")
     assert True
 
-<<<<<<< HEAD
-# def test_blob():
-
-#     client = TuioClient()
-
-#     i = 0
-#     blob = Blob(13)
-
-#     blob.velocity             = (0.2,0.1)
-#     blob.motion_acceleration  = 0.1 
-
-#     client.blobs.append(blob)
-#     while i < 10:
-
-
-#         i+=1
-#         blob.position  = (0.1+0.01*i,0.2)
-
-#         client.send_bundle()
-#         print("sented message")
-#     assert True
-=======
 def test_blob():
 
     client = TuioClient()
@@ -79,7 +57,6 @@ def test_blob():
         client.send_bundle()
         print("sented message")
     assert True
->>>>>>> feature/pip
 
 def test_object():
     client = TuioClient()
