e093409007e7a2972c3f7cc105b932c5ff1b09db
Thomas Weigel <thomas.weigel@telekom.de>
2020-05-23 20:23:25 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-tuio", # Replace with your own username
+    version="0.0.1",
+    author="tweigel-dev",
+    author_email="weigel-thomas@outlook.com",
+    description="python3 implementation of the TUIO protocol",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tweigel-dev/python-tuio",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
