212bd7bd4c6233f49c94d782bf4927a080bb21fa
Thomas Weigel <thomas.weigel@telekom.de>
2020-05-26 11:25:42 +0200

    exclude profiles into own file

--- test/tuio_test.py
+++ test/tuio_test.py
@@ -1,8 +1,8 @@
 
 from pythontuio.tuio import TuioServer
-from pythontuio.tuio import Cursor
-from pythontuio.tuio import Blob
-from pythontuio.tuio import Object
+from pythontuio.tuio_profiles import Cursor
+from pythontuio.tuio_profiles import Blob
+from pythontuio.tuio_profiles import Object
 
 
 
