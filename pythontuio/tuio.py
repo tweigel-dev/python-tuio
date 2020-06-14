6c32948c39cff339b3c51f483f28910bfcc447e3
Thomas Weigel <thomas.weigel@telekom.de>
2020-06-14 16:33:23 +0200

    fix issue of listening on __init__. now listening on start()

--- pythontuio/tuio.py
+++ pythontuio/tuio.py
@@ -24,7 +24,6 @@ from pythontuio.dispatcher import TuioDispatcher
 
 
 
-
 class TuioClient(TuioDispatcher, BlockingOSCUDPServer): # pylint: disable=too-many-ancestors
     """
     The TuioClient class is the central TUIO protocol decoder component.
@@ -36,22 +35,14 @@ class TuioClient(TuioDispatcher, BlockingOSCUDPServer): # pylint: disable=too-ma
     def __init__(self, server_address: Tuple[str, int]):
         TuioDispatcher.__init__(self)
         self._dispatcher = self
-        BlockingOSCUDPServer.__init__(self,server_address, self)
         self.connected = False
-
-
-    def server_bind(self):
-        print(f"starting tuio-client at port {self.server_address[1]}")
-        super().server_bind()
-        self.handle_request()
-
-    def server_close(self):
-        print("stopping tuio-client")
-        super().server_close()
+        self.server_address = server_address
     def start(self):
         """
         start serving for UDP OSC packages
         """
+        print(f"starting tuio-client at port {self.server_address[1]}")
+        BlockingOSCUDPServer.__init__(self,self.server_address, self)
         self.serve_forever()
 
 class TuioServer(TuioDispatcher, UDPClient):
