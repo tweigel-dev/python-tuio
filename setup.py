13c084070f1d03e6c60b4f46aa089e1f34ca52bc
tweigel-dev <56434333+tweigel-dev@users.noreply.github.com>
2020-08-22 22:38:39 +0200

    Update setup.py
    increment version for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-tuio",
-    version="0.0.7",
+    version="0.0.8",
     author="tweigel-dev",
     author_email="weigel-thomas@outlook.de",
     description="python3 implementation of the TUIO protocol",
