00f8a359ae20c95037b7bcd63be3650d27e573d7
Marcus Ding <marcus.ding@student.hpi.uni-potsdam.de>
2021-11-13 19:26:28 +0100

    Update const.py

--- pythontuio/const.py
+++ pythontuio/const.py
@@ -5,6 +5,8 @@ File for constant TUIO values
 TUIO_CURSOR = "/tuio/2Dcur"
 TUIO_OBJECT = "/tuio/2Dobj"
 TUIO_BLOB   = "/tuio/2Dblb"
+PHARUS_PATH = "/track"
+PHARUS_FRAME = "/frame"
 
 TUIO_ALIVE = "alive"
 TUIO_SET =  "set"
